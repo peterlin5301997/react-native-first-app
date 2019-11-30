8ea672b023cca729a616f61620559c389e4bc7f9
Peter Lin <peterlin5301997@gmail.com>
2019-11-30 15:33:51 -0500

    adds AddGoal function

--- Development/Module6/react-native-first-app/App.js
+++ Development/Module6/react-native-first-app/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('') // React Hook
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputChange = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoal = () => {
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal])
+  }
+
   return(
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input}/>
-        <Button title="ADD" />
+        <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputChange} />
+        <Button title="ADD" onPress={addGoal} />
+      </View>
+      <View>
+        {courseGoals.map((goal) => <Text>{goal}</Text>)}
       </View>
     </View>
   )
