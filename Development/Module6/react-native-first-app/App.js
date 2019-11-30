fab3532c15261c44fc502ac3f8af766306398d35
Peter Lin <peterlin5301997@gmail.com>
2019-11-30 15:20:33 -0500

    adds StyleSheet

--- Development/Module6/react-native-first-app/App.js
+++ Development/Module6/react-native-first-app/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import ChangeText from './components/ChangeText.js';
-import AddCourse from './components/AddCourse.js';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return(
-    <AddCourse />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input}/>
+        <Button title="ADD" />
+      </View>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    width: '80%'
+  }
+})
